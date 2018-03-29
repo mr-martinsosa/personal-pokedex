e51e5ab9b4d28e5e8286c246ec864ceea7055458
Martin Sosa <mr.martinsosa@gmail.com>
2018-03-29 15:18:52 -0400

    updated pokemon classes and added ajax calls

--- sause.js
+++ sause.js
@@ -1,23 +1,42 @@
 class Pokemon{
-	constructor(hp, attack, defense, abilities = []){
-		this.hp = hp
-		this.attack = attack
-		this.defense = defense
-		this.abilities = abilities
+	constructor(dex_num){
+		this.dex_num = dex_num
+
+		axios.get(`https://pokeapi.co/api/v2/pokemon-form/${this.dex_num}/`)
+		.then((response) => 
+			let data = response.data
+			this.name = data.forms[0].name
+			this.hp = data.stats[5].base_stat
+			this.attack = data.stats[4].base_stat
+			this.defense = data.stats[3].base_stat
+				
+			for(let i = 0; i < data.abilities i++){
+				this.abilities.push(data.abilities[i].ability.name)
+			}
+
+			this.sprite = data.sprites.front_default
+		})
 	}
 }
 
 class Sause{
 	constructor(pokemon){
+		//initialized with array of pokemon
 		this.pokemon = pokemon
 	}
 
 	all(){
+		//return array of pokemon objects
 		return this.pokemon
 	}
 
-	get(name){
+	get(pokemon){
 		// return pokemon retrieved from ajax
+		for(let i = 0; i < this.pokemon.length; i++){
+			if(pokemon === this.pokemon[i].name){
+				return pokemon[i]
+			}
+		}
 	}
 }
 
