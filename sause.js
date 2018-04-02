9e16be967716fe98442ef65ec645406c1623472e
Martin Sosa <mr.martinsosa@gmail.com>
2018-04-02 13:05:17 -0400

    edited font, fixed pokemon layout

--- sause.js
+++ sause.js
@@ -1,5 +1,5 @@
 
-let check = [];
+let check = 0; //set counter for base intialization
 
 class Pokemon{
 	constructor(dex_num){
@@ -7,27 +7,22 @@ class Pokemon{
 
 		axios.get(`https://pokeapi.co/api/v2/pokemon/${this.dex_num}/`)
 		.then((response) => {
-			check.push(true);
+			check++ //increment on creation
 
 			let info = response.data
-			this["name"] = info.forms[0].name
+			this["name"] = (info.forms[0].name).charAt(0).toUpperCase() + (info.forms[0].name).slice(1)
 			this["hp"] = info.stats[5].base_stat
 			this["attack"] = info.stats[4].base_stat
 			this["defense"] = info.stats[3].base_stat
-				
-			for(let i = 0; i < info.abilities; i++){
-				this.abilities.push(info.abilities[i].ability.name)
-				
-
+			this["abilities"] = []
+			for(let i = 0; i < info.abilities.length; i++){
+				this["abilities"].push( (info.abilities[i].ability.name).charAt(0).toUpperCase() + (info.abilities[i].ability.name).slice(1) )
 			}
 
 			this["sprite"] = info.sprites.front_default
-			// console.log(check);
 			
-			// console.log(dragonite);
-			
-
-		checkMe();})
+		checkMe()
+		}) //call checkMe()
 	}
 
 }
@@ -65,7 +60,7 @@ let pokemonThreeImg = document.getElementById("pokemon-three-img")
 
 
 function checkMe(){
-	if (check.length === 3){
+	if (check === 3 ){ //if called 3 times, all the pokemon have been created so do everythingElse
 		everythingElse();
 	}
 }
@@ -73,80 +68,80 @@ function checkMe(){
 
 function everythingElse(){
 
-let title = document.querySelector("#random-title")
+	let title = document.querySelector("#random-title")
 
-listOfTitles = [
-	"Lorekeeper",
-	"Pokemon Trainer",
-	"Leader",
-	"Elite Four",
-	"Champion",
-	"Ace Trainer",
-	"Dragon Tamer",
-	"Black Belt",
-	"Rival",
-	"Youngster",
-	"Swimmer",
-	"Tamer",
-	"Cook",
-	"Captain",
-	"Fisherman",
-	"World Champion",
-	"Pikachu Fan",
-	"Arcade Star",
-	"Elder",
-	"Chaser",
-	"Rogue",
-	"Worker",
-	"Supertrainer",
-	"Teacher",
-	"Lone Wolf"
-]
+	listOfTitles = [
+		"Lorekeeper",
+		"Pokemon Trainer",
+		"Leader",
+		"Elite Four",
+		"Champion",
+		"Ace Trainer",
+		"Dragon Tamer",
+		"Black Belt",
+		"Rival",
+		"Youngster",
+		"Swimmer",
+		"Tamer",
+		"Cook",
+		"Captain",
+		"Fisherman",
+		"World Champion",
+		"Pikachu Fan",
+		"Arcade Star",
+		"Elder",
+		"Chaser",
+		"Rogue",
+		"Worker",
+		"Supertrainer",
+		"Teacher",
+		"Lone Wolf"
+	]
 
-title.innerText = listOfTitles[Math.floor(Math.random() * Math.floor(listOfTitles.length) )] + " Sause"
-
-
-pokemonOneImg.src = dragonite.sprite
-pokemonTwoImg.src = porygonZ.sprite
-pokemonThreeImg.src = scrafty.sprite
-
-// let pokemonInfo = document.querySelector("#pokemon-one-info")
-let pokemonOneInfo = document.querySelector("#pokemon-one-info")
-let pokemonTwoInfo = document.querySelector("#pokemon-two-info")
-let pokemonThreeInfo = document.querySelector("#pokemon-three-info")
-
-pokemonOneInfo.innerHTML = `${dragonite.name}: <br/>
-							HP: ${dragonite.hp} <br/>
-							Attack: ${dragonite.attack} <br/>
-							Defense: ${dragonite.defense} <br/>
-							`
-							// Abilities: ${dragonite.abilities[0]} , ${dragonite.abilities[1]} <br/>
-
-pokemonTwoInfo.innerHTML = `${porygonZ.name}: <br/>
-							HP: ${porygonZ.hp} <br/>
-							Attack: ${porygonZ.attack} <br/>
-							Defense: ${porygonZ.defense} <br/>
-							`
-							// Abilities: ${porygonZ.abilities[0]} , ${porygonZ.abilities[1]} <br/>
-
-pokemonThreeInfo.innerHTML = `${scrafty.name}: <br/>
-							HP: ${scrafty.hp} <br/>
-							Attack: ${scrafty.attack} <br/>
-							Defense: ${scrafty.defense} <br/>
-							`
-							// Abilities: ${scrafty.abilities[0]} , ${scrafty.abilities[1]} <br/>
+	title.innerText = listOfTitles[Math.floor(Math.random() * Math.floor(listOfTitles.length) )] + " Sause"
 
 
-let party = [dragonite, porygonZ, scrafty]
+	pokemonOneImg.src = dragonite.sprite
+	pokemonTwoImg.src = porygonZ.sprite
+	pokemonThreeImg.src = scrafty.sprite
 
-let sause = new Sause(party)
+	console.log(dragonite)
+	// let pokemonInfo = document.querySelector("#pokemon-one-info")
+	let pokemonOneInfo = document.querySelector("#pokemon-one-info")
+	let pokemonTwoInfo = document.querySelector("#pokemon-two-info")
+	let pokemonThreeInfo = document.querySelector("#pokemon-three-info")
+
+	pokemonOneInfo.innerHTML = `<u>${dragonite.name}</u> <br/>
+								<u>HP</u>: ${dragonite.hp} <br/>
+								<u>Attack</u>: ${dragonite.attack} <br/>
+								<u>Defense</u>: ${dragonite.defense} <br/>
+								<u>Abilities</u>: <br/> ${dragonite.abilities[0]} <br/> ${dragonite.abilities[1]} <br/>
+								`
+
+	pokemonTwoInfo.innerHTML = `<u>${porygonZ.name}</u> <br/>
+								<u>HP</u>: ${porygonZ.hp} <br/>
+								<u>Attack</u>: ${porygonZ.attack} <br/>
+								<u>Defense</u>: ${porygonZ.defense} <br/>
+								<u>Abilities</u>: <br/> ${porygonZ.abilities[0]} <br/> ${porygonZ.abilities[1]} <br/>
+								`
+
+	pokemonThreeInfo.innerHTML = `<u>${scrafty.name}</u> <br/>
+								<u>HP</u>: ${scrafty.hp} <br/>
+								<u>Attack</u>: ${scrafty.attack} <br/>
+								<u>Defense</u>: ${scrafty.defense} <br/>
+								<u>Abilities</u>: <br/> ${scrafty.abilities[0]} <br/> ${scrafty.abilities[1]} <br/>
+								`
+
+	let party = [dragonite, porygonZ, scrafty]
+
+	let sause = new Sause(party)
 
 }
-// for(let i = 0; i < sause.pokemon.length; i++){
-// 	let pokemonInfo = document.querySelector(`#pokemon-${i}-info`)
-// 	pokemonInfo.innerHTML = `${sause.pokemon[i].name}: <br/>
-// 							HP: ${sause.pokemon[i].hp} <br/>
-// 							Attack: ${sause.pokemon[i].attack} <br/>
-// 							Defense: ${sause.pokemon[i].defense} <br/>
-// 							`
-// }
+	// for(let i = 0; i < sause.pokemon.length; i++){
+	// 	let pokemonInfo = document.querySelector(`#pokemon-${i}-info`)
+	// 	pokemonInfo.innerHTML = `${sause.pokemon[i].name}: <br/>
+	// 							HP: ${sause.pokemon[i].hp} <br/>
+	// 							Attack: ${sause.pokemon[i].attack} <br/>
+	// 							Defense: ${sause.pokemon[i].defense} <br/>
+	// 							`
+	// }
