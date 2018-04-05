d2d65ba040ded2b877abaf95ae1a9c9c875520bb
Martin Sosa <mr.martinsosa@gmail.com>
2018-04-05 10:47:42 -0400

    Changed pokemon call from itself to trainer

--- sause.js
+++ sause.js
@@ -29,7 +29,7 @@ class Pokemon {
 class Sause {
     constructor(pokemon) {
         //initialized with array of pokemon
-        this.pokemon = []
+        this.pokemon = pokemon
     }
 
     all() {
@@ -129,48 +129,94 @@ function main() {
 
     console.log(dragonite)
     // let pokemonInfo = document.querySelector("#pokemon-one-info")
-    let pokemonOneInfo = document.querySelector("#pokemon-one-info")
-    let pokemonTwoInfo = document.querySelector("#pokemon-two-info")
-    let pokemonThreeInfo = document.querySelector("#pokemon-three-info")
+    let pokemonOneInfo = document.querySelector("#pokemon-1-info")
+    let pokemonTwoInfo = document.querySelector("#pokemon-2-info")
+    let pokemonThreeInfo = document.querySelector("#pokemon-3-info")
 
-    pokemonOneInfo.innerHTML = `<u>${dragonite.name}</u> <br/>
-								<u>HP</u>: ${dragonite.hp} <br/>
-								<u>Attack</u>: ${dragonite.attack} <br/>
-								<u>Defense</u>: ${dragonite.defense} <br/>
-								<u>Abilities</u>: <br/> ${dragonite.abilities[0]} <br/> ${dragonite.abilities[1]} <br/>
-								`
+    // pokemonOneInfo.innerHTML = `<u>${dragonite.name}</u> <br/>
+				// 				<u>HP</u>: ${dragonite.hp} <br/>
+				// 				<u>Attack</u>: ${dragonite.attack} <br/>
+				// 				<u>Defense</u>: ${dragonite.defense} <br/>
+				// 				<u>Abilities</u>: <br/> ${dragonite.abilities[0]} <br/> ${dragonite.abilities[1]} <br/>
+				// 				`
 
-    pokemonTwoInfo.innerHTML = `<u>${porygonZ.name}</u> <br/>
-								<u>HP</u>: ${porygonZ.hp} <br/>
-								<u>Attack</u>: ${porygonZ.attack} <br/>
-								<u>Defense</u>: ${porygonZ.defense} <br/>
-								<u>Abilities</u>: <br/> ${porygonZ.abilities[0]} <br/> ${porygonZ.abilities[1]} <br/>
-								`
+    // pokemonTwoInfo.innerHTML = `<u>${porygonZ.name}</u> <br/>
+				// 				<u>HP</u>: ${porygonZ.hp} <br/>
+				// 				<u>Attack</u>: ${porygonZ.attack} <br/>
+				// 				<u>Defense</u>: ${porygonZ.defense} <br/>
+				// 				<u>Abilities</u>: <br/> ${porygonZ.abilities[0]} <br/> ${porygonZ.abilities[1]} <br/>
+				// 				`
 
-    pokemonThreeInfo.innerHTML = `<u>${scrafty.name}</u> <br/>
-								<u>HP</u>: ${scrafty.hp} <br/>
-								<u>Attack</u>: ${scrafty.attack} <br/>
-								<u>Defense</u>: ${scrafty.defense} <br/>
-								<u>Abilities</u>: <br/> ${scrafty.abilities[0]} <br/> ${scrafty.abilities[1]} <br/>
-								`
+    // pokemonThreeInfo.innerHTML = `<u>${scrafty.name}</u> <br/>
+				// 				<u>HP</u>: ${scrafty.hp} <br/>
+				// 				<u>Attack</u>: ${scrafty.attack} <br/>
+				// 				<u>Defense</u>: ${scrafty.defense} <br/>
+				// 				<u>Abilities</u>: <br/> ${scrafty.abilities[0]} <br/> ${scrafty.abilities[1]} <br/>
+				// 				`
 
-    let elem = document.querySelector('.modal') //grab modal
-    elem.addEventListener("click", (event) => {
-        let instance = M.Modal.init(elem, options) //initialize modal
-        instance = M.Modal.getInstance(elem)
-        instance.open()
-    })
+    // let elem = document.querySelector('.modal') //grab modal
+    // elem.addEventListener("click", (event) => {
+    //     let instance = M.Modal.init(elem, options) //initialize modal
+    //     instance = M.Modal.getInstance(elem)
+    //     instance.open()
+    // })
 
     let party = [dragonite, porygonZ, scrafty]
 
     let sause = new Sause(party)
 
+    grabAll = sause.all()
+
+    // pokemonOneInfo.innerHTML = `<u>${sause.get(dragonite).name}</u> <br/>
+				// 				<u>HP</u>: ${sause.get(dragonite).hp} <br/>
+				// 				<u>Attack</u>: ${sause.get(dragonite).attack} <br/>
+				// 				<u>Defense</u>: ${sause.get(dragonite).defense} <br/>
+				// 				<u>Abilities</u>: <br/> ${sause.get(dragonite).abilities[0]} <br/> ${sause.get(dragonite).abilities[1]} <br/>
+				// 				`
+
+    // pokemonTwoInfo.innerHTML = `<u>${sause.get(porygon-z).name}</u> <br/>
+				// 				<u>HP</u>: ${sause.get(porygon-z).hp} <br/>
+				// 				<u>Attack</u>: ${sause.get(porygon-z).attack} <br/>
+				// 				<u>Defense</u>: ${sause.get(porygon-z).defense} <br/>
+				// 				<u>Abilities</u>: <br/> ${sause.get(porygon-z).abilities[0]} <br/> ${sause.get(porygon-z).abilities[1]} <br/>
+				// 				`
+
+    // pokemonThreeInfo.innerHTML = `<u>${sause.get(scrafty).name}</u> <br/>
+				// 				<u>HP</u>: ${sause.get(scrafty).hp} <br/>
+				// 				<u>Attack</u>: ${sause.get(scrafty).attack} <br/>
+				// 				<u>Defense</u>: ${sause.get(scrafty).defense} <br/>
+				// 				<u>Abilities</u>: <br/> ${sause.get(scrafty).abilities[0]} <br/> ${sause.get(scrafty).abilities[1]} <br/>
+				// 				`
+
+for(let i = 0; i < grabAll.length; i++){
+	let pokemonInfo = document.querySelector(`#pokemon-${i+1}-info`)
+	if(i === 0){
+		pokemonOneInfo.innerHTML = `
+							<u>${sause.pokemon[i].name}:</u> <br/>
+								<u>HP</u>: ${sause.pokemon[i].hp} <br/>
+								<u>Attack</u>: ${sause.pokemon[i].attack} <br/>
+								<u>Defense</u>: ${sause.pokemon[i].defense} <br/>
+								<u>Abilities</u>: <br/> ${sause.pokemon[i].abilities[0]} <br/> ${sause.pokemon[i].abilities[1]} <br/>
+								`
+	}
+	if(i === 1){
+		pokemonTwoInfo.innerHTML = `
+							<u>${sause.pokemon[i].name}:</u> <br/>
+								<u>HP</u>: ${sause.pokemon[i].hp} <br/>
+								<u>Attack</u>: ${sause.pokemon[i].attack} <br/>
+								<u>Defense</u>: ${sause.pokemon[i].defense} <br/>
+								<u>Abilities</u>: <br/> ${sause.pokemon[i].abilities[0]} <br/> ${sause.pokemon[i].abilities[1]} <br/>
+								`
+	}
+	if(i === 2){
+		pokemonThreeInfo.innerHTML = `
+							<u>${sause.pokemon[i].name}:</u> <br/>
+								<u>HP</u>: ${sause.pokemon[i].hp} <br/>
+								<u>Attack</u>: ${sause.pokemon[i].attack} <br/>
+								<u>Defense</u>: ${sause.pokemon[i].defense} <br/>
+								<u>Abilities</u>: <br/> ${sause.pokemon[i].abilities[0]} <br/> ${sause.pokemon[i].abilities[1]} <br/>
+								`
+	}
+ }
+
 }
-// for(let i = 0; i < sause.pokemon.length; i++){
-// 	let pokemonInfo = document.querySelector(`#pokemon-${i}-info`)
-// 	pokemonInfo.innerHTML = `${sause.pokemon[i].name}: <br/>
-// 							HP: ${sause.pokemon[i].hp} <br/>
-// 							Attack: ${sause.pokemon[i].attack} <br/>
-// 							Defense: ${sause.pokemon[i].defense} <br/>
-// 							`
-// }
